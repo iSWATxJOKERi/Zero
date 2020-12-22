24cddd8b7c0e6aac2260dcd78cb32d5678288bb6
swatjoker <iSWATxJOKERi>
2020-12-22 13:49:39 -0500

    Created login routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
 
 const users =  require("./routes/api/users");
 const items = require("./routes/api/items");
@@ -9,6 +10,8 @@ const items = require("./routes/api/items");
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("Connected to MongoDB succesfully")).catch(err => console.log(err))
 
 app.get("/", (req, res) => res.send("Facts"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use("/api/users", users);
 app.use("/api/items", items);
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,48 @@
+const jwt = require('jsonwebtoken');
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
+const keys = require('../../config/keys');
 
 router.get("/test", (req, res) => res.json({ msg: "This is a users route" }));
+router.post("/register", (req, res) => {
+    User.findOne({ handle: req.body.handle }).then(user => {
+        if(user) {
+            return res.status(400).json({ handle: "A user already has this name."})
+        } else {
+            const newUser = new User({
+                handle: req.body.handle,
+                password: req.body.password
+            })
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if(err) throw err;
+                    newUser.password = hash;
+                    newUser.save().then(user => res.json(user)).catch(err => console.log(err));
+                })
+            })
+        }
+    })
+});
+router.post("/login", (req, res) => {
+    const handle = req.body.handle;
+    const password = req.body.password;
+
+    User.findOne({ handle }).then(user => {
+        if(!user) {
+            return res.status(404).json({ handle: "This user does not exist"});
+        }
+
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if(isMatch) {
+                res.json({ msg: 'Success'});
+            } else {
+                return res.status(400).json({ credentials: 'Incorrect Password or Handle'});
+            }
+        })
+    })
+})
 
 module.exports = router;
